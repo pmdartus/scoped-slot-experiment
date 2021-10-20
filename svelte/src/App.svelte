64c6812010f13d919e0c1242dd09b33371d00b1b
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-10-20 17:42:22 +0200

    Add svelte example

---
+++ svelte/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import List from "./lib/List.svelte";
+
+  let todos = [
+    { id: 1, name: "Buy milk", done: true },
+    { id: 2, name: "File taxes", done: false },
+    { id: 3, name: "Fix bug in production", done: true },
+  ];
+
+  function addTodo() {
+    todos = [
+      ...todos,
+      {
+        id: todos.length + 1,
+        name: "Do something else",
+        done: true,
+      },
+    ];
+  }
+</script>
+
+<main>
+  <button on:click="{addTodo}">Add todo</button>
+
+  <List items={todos} let:item={item}>
+    <input type="checkbox" checked={item.done} id={`item1-${item.id}`} />
+    <label for={`item1-${item.id}`}>{item.name}</label>
+  </List>
+</main>
---
+++ svelte/src/lib/List.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let items;
+</script>
+
+<ul>
+  {#each items as item (item.id)}
+  <li>
+    <slot item={item}></slot>
+  </li>
+  {/each}
+</ul>
